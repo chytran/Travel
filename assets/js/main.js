6ac7abbc5e6bff226dde35cf024922b11ca56922
chytran <chytran@users.noreply.github.com>
2021-06-29 12:05:37 -0700

    home section in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,13 @@ if(navClose){
         navMenu.classList.remove("show-menu")
     })
 }
+
+/*==================== REMOVE MENU MOBILE ===================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    const navMenu = document.querySelector(".nav__menu");
+    // When we click on each nav__link, it removes the next
+    navMenu.classList.remove("show-menu")
+}
+navLink.forEach(n => n.addEventListener("click", linkAction))
