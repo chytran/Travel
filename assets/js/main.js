edbdb10478dd08fd9d22f82954cc28504b6078fb
chytran <chytran@users.noreply.github.com>
2021-06-30 11:54:21 -0700

    Home and About portions are done

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,11 @@ function linkAction() {
     navMenu.classList.remove("show-menu")
 }
 navLink.forEach(n => n.addEventListener("click", linkAction))
+
+/*================== CHANGE HEADER BACKGROUND COLOR ON SCROLL =====================*/
+function scrollHeader() {
+    const header = document.getElementById("header");
+    // When scroll is greater than 100, add scroll-header class
+    if(this.scrollY >= 100) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader)
