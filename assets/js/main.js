5c9d17e4f23fe5c67f8b8dd75531f76b500fc226
chytran <chytran@users.noreply.github.com>
2021-07-01 12:21:37 -0700

    Discover portion completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,5 @@ function scrollHeader() {
     if(this.scrollY >= 100) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader)
+
+/*==================== SWIPER IN DISCOVER ===================*/
